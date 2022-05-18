f6603da8dcd1dc6dd0eeaffac1118b8b1dbf942a
dongchan <dongchan@qarasoft.com>
2022-05-18 22:01:27 +0900

    fix: json content-type

--- src/main/java/coinbuying/userservice/router/UserRouter.java
+++ src/main/java/coinbuying/userservice/router/UserRouter.java
@@ -3,11 +3,11 @@ package coinbuying.userservice.router;
 import coinbuying.userservice.handler.UserHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration(proxyBeanMethods = false)
@@ -15,10 +15,11 @@ public class UserRouter {
     @Bean
     public RouterFunction<ServerResponse> userPathRouter(UserHandler userHandler) {
         return RouterFunctions
-                .route(POST("/user/signup"), userHandler::memberRegistration)      // 회원가입
-                .andRoute(POST("/user/login"), userHandler::login)       // 로그인
-                .andRoute(GET("/user/{userId}"), userHandler::findMemberInfo)        // 유저정보조회
-                .andRoute(GET("/user"), userHandler::health)
-                ;
+                .route()
+                .POST("/user/signup", accept(APPLICATION_JSON), userHandler::memberRegistration)// 회원가입
+                .POST("/user/login", accept(APPLICATION_JSON), userHandler::login)       // 로그인
+                .GET("/user/{userId}", userHandler::findMemberInfo)        // 유저정보조회
+                .GET("/user", userHandler::health)
+                .build();
     }
 }
